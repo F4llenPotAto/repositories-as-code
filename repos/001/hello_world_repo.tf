a091146aa666cc6159b71c4603742b689533ee17
Trenton Ornelas <trentonornelas@gmail.com>
2023-08-21 18:35:48 -0500

    feat: enable branch protection

--- repos/001/hello_world_repo.tf
+++ repos/001/hello_world_repo.tf
@@ -7,3 +7,44 @@ resource "github_repository" "first_repo_test" {
   vulnerability_alerts = true
 
 }
+
+resource "github_branch_protection" "example" {
+  repository_id = github_repository.first_repo_test.name
+
+  pattern          = "master"
+  enforce_admins   = true
+  allows_deletions = true
+
+  required_status_checks {
+    #  strict   = false
+    #  contexts = ["ci/travis"]
+  }
+
+  required_pull_request_reviews {
+    #  dismiss_stale_reviews  = true
+    #  restrict_dismissals    = true
+    #  dismissal_restrictions = [
+    #    data.github_user.example.node_id,
+    #    github_team.example.node_id,
+    #    "/exampleuser",
+    #    "exampleorganization/exampleteam",
+    #  ]
+  }
+
+  push_restrictions = [
+    #    data.github_user.example.node_id,
+    #    "/exampleuser",
+    #    "exampleorganization/exampleteam",
+    # limited to a list of one type of restriction (user, team, app)
+    # github_team.example.node_id
+  ]
+
+  force_push_bypassers = [
+    #    data.github_user.example.node_id,
+    #    "/exampleuser",
+    #    "exampleorganization/exampleteam",
+    # limited to a list of one type of restriction (user, team, app)
+    # github_team.example.node_id
+  ]
+
+}
