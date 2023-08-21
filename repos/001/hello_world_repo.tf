ace733eb7b0df16352b451afa44b6c59613ac6e9
Trenton Ornelas <trentonornelas@gmail.com>
2023-08-20 22:59:22 -0500

    fix: address checkov scan failures

--- repos/001/hello_world_repo.tf
+++ repos/001/hello_world_repo.tf
@@ -2,6 +2,8 @@ resource "github_repository" "first_repo_test" {
   name        = "smogdog-repo"
   description = "First attempt at creating a github repo using Terraform"
 
-  visibility = "public"
+  visibility = "private"
+
+  vulnerability_alerts = true
 
 }
