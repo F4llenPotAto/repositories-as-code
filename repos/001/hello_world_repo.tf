6cd67c98f4faaebb9e3a0d3e7d615623d3d06513
Trenton Ornelas <trentonornelas@gmail.com>
2023-08-21 19:07:17 -0500

    feat: require signed commits

--- repos/001/hello_world_repo.tf
+++ repos/001/hello_world_repo.tf
@@ -2,49 +2,20 @@ resource "github_repository" "first_repo_test" {
   name        = "smogdog-repo"
   description = "First attempt at creating a github repo using Terraform"
 
-  visibility = "private"
+  visibility = "public"
 
   vulnerability_alerts = true
 
 }
 
-resource "github_branch_protection" "example" {
-  repository_id = github_repository.first_repo_test.name
+resource "github_branch_protection_v3" "master_branch_protection" {
+  repository = github_repository.first_repo_test.name
+  branch     = "master"
 
-  pattern          = "master"
-  enforce_admins   = true
-  allows_deletions = true
+  require_signed_commits = true
 
-  required_status_checks {
-    #  strict   = false
-    #  contexts = ["ci/travis"]
+  restrictions {
+    users = ["foo-user"]
   }
-
-  required_pull_request_reviews {
-    #  dismiss_stale_reviews  = true
-    #  restrict_dismissals    = true
-    #  dismissal_restrictions = [
-    #    data.github_user.example.node_id,
-    #    github_team.example.node_id,
-    #    "/exampleuser",
-    #    "exampleorganization/exampleteam",
-    #  ]
-  }
-
-  push_restrictions = [
-    #    data.github_user.example.node_id,
-    #    "/exampleuser",
-    #    "exampleorganization/exampleteam",
-    # limited to a list of one type of restriction (user, team, app)
-    # github_team.example.node_id
-  ]
-
-  force_push_bypassers = [
-    #    data.github_user.example.node_id,
-    #    "/exampleuser",
-    #    "exampleorganization/exampleteam",
-    # limited to a list of one type of restriction (user, team, app)
-    # github_team.example.node_id
-  ]
-
 }
+
