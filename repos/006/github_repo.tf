632ab887ddb5aad9ed6823bf6c71dd6732e39d6a
Trenton Ornelas <trentonornelas@gmail.com>
2023-11-28 16:04:49 -0600

    feat: sync with remote

---
+++ repos/006/github_repo.tf
@@ -0,0 +1,18 @@
+resource "github_repository" "this" {
+  name        = local.github_repository_name
+  description = local.github_repository_description
+
+  visibility = "public"
+
+  vulnerability_alerts = true
+
+}
+
+resource "github_branch_protection" "master_branch_protection" {
+  repository_id = github_repository.this.name
+  pattern       = local.protected_main_branch_name
+
+  require_signed_commits = true
+
+}
+
